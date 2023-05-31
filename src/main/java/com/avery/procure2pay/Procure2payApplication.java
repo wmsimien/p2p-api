b1927356d11e48c913a2bf39574fc9489fab6aac
wmavery <wanda.avery@att.net>
2023-05-31 18:14:59 -0500

    initial setup

---
+++ src/main/java/com/avery/procure2pay/Procure2payApplication.java
@@ -0,0 +1,13 @@
+package com.avery.procure2pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Procure2payApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Procure2payApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avery/procure2pay/Procure2payApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avery.procure2pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Procure2payApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
