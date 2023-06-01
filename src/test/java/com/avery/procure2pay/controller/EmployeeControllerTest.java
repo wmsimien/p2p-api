b787d0292aa91bc86199d65e64db03baf36048f9
wmavery <wanda.avery@att.net>
2023-05-31 21:32:33 -0500

    implement test for hello() test

---
+++ src/test/java/com/avery/procure2pay/controller/EmployeeControllerTest.java
@@ -0,0 +1,27 @@
+package com.avery.procure2pay.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EmployeeController.class)
+class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void hello_success() throws Exception {
+        mockMvc.perform(get("/api/hello/"))
+        .andExpect(content().string("Hello, Family"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
